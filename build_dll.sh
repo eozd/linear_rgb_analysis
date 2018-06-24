75e01db187095c34b7ed4e386f31de40b789654a
Esref Ozdemir <esref.ozdemir27@gmail.com>
2018-06-24 13:17:02 +0300

    Move header files to include folder

--- build_dll.sh
+++ build_dll.sh
@@ -2,5 +2,5 @@
 
 SRC="src/linear_regression.c src/rgb_analysis.c src/utils.c"
 
-gcc -m32 -DLINUX -O3 -shared -fPIC $SRC -lm -o build/rgb_analysis.so
-i686-w64-mingw32-gcc -DWINDOWS -static-libgcc -O3 -shared -fPIC $SRC -lm -o build/rgb_analysis.dll
+gcc -m32 -DLINUX -O3 -shared -fPIC -Iinclude $SRC -lm -o build/rgb_analysis.so
+i686-w64-mingw32-gcc -DWINDOWS -static-libgcc -O3 -shared -fPIC -Iinclude $SRC -lm -o build/rgb_analysis.dll
